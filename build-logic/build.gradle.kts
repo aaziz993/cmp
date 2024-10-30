efac57a43e144dbeb0f5a212289f4de5b76b024f
Aziz <a.atoev93@gmail.com>
2024-10-30 11:42:06 +0500

    Initial Commit

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -35,49 +35,51 @@ dependencies {
     implementation(libs.plugins.gradle.pre.commit.git.hooks.toDep())
     // publishing
     implementation(libs.plugins.vanniktech.maven.publish.toDep())
-    runtimeOnly(libs.plugins.vanniktech.maven.publish.toDep())
+//    runtimeOnly(libs.plugins.vanniktech.maven.publish.toDep())
 
     // Kotlin
-    runtimeOnly(libs.plugins.kotlin.serialization.toDep())
     runtimeOnly(libs.plugins.ksp.toDep())
     // generate no arg contructor by specified annotation
     implementation(libs.plugins.noarg.toDep())
-    runtimeOnly(libs.plugins.noarg.toDep())
+//    runtimeOnly(libs.plugins.noarg.toDep())
     // make class open for inheritance by specified annotation
     implementation(libs.plugins.allopen.toDep())
-    runtimeOnly(libs.plugins.allopen.toDep())
+//    runtimeOnly(libs.plugins.allopen.toDep())
+    // serialization
+    runtimeOnly(libs.plugins.kotlin.serialization.toDep())
     // multiplatform
     implementation(libs.plugins.kotlin.multiplatform.toDep())
     // compiler processor for generating code during compilation
     implementation(libs.plugins.ksp.toDep())
     // generate coverage report
     implementation(libs.plugins.kover.toDep())
-    runtimeOnly(libs.plugins.kover.toDep())
+//    runtimeOnly(libs.plugins.kover.toDep())
     // code format check and fix
     implementation(libs.plugins.spotless.toDep())
-    runtimeOnly(libs.plugins.spotless.toDep())
+//    runtimeOnly(libs.plugins.spotless.toDep())
     // code analysis
     implementation(libs.plugins.sonarqube.toDep())
-    runtimeOnly(libs.plugins.sonarqube.toDep())
+//    runtimeOnly(libs.plugins.sonarqube.toDep())
     // documentation
     implementation(libs.plugins.dokka.toDep())
-    runtimeOnly(libs.plugins.dokka.toDep())
+//    runtimeOnly(libs.plugins.dokka.toDep())
     // provides a repository for downloading JVMs
     implementation(libs.plugins.binary.compatibility.validator.toDep())
     // the tool that produces Kotlin source example files and tests from markdown documents with embedded snippets of Kotlin code
     implementation(libs.plugins.knit.toDep())
+    // jvm
     runtimeOnly(libs.plugins.kotlin.jvm.toDep())
     // data pipleline processing
     runtimeOnly(libs.plugins.dataframe.toDep())
     // providing detailed failure messages with contextual information during testing.
     implementation(libs.plugins.power.assert.toDep())
-    runtimeOnly(libs.plugins.power.assert.toDep())
+//    runtimeOnly(libs.plugins.power.assert.toDep())
     // testing
     runtimeOnly(libs.plugins.kotest.multiplatform.toDep())
 
     // Compose multiplatform
     implementation(libs.plugins.compose.multiplatform.toDep())
-    runtimeOnly(libs.plugins.compose.multiplatform.toDep())
+//    runtimeOnly(libs.plugins.compose.multiplatform.toDep())
     runtimeOnly(libs.plugins.compose.compiler.toDep())
 
     // Android
@@ -93,22 +95,22 @@ dependencies {
 
     // SQLDelight
     implementation(libs.plugins.sqldelight.toDep())
-    runtimeOnly(libs.plugins.sqldelight.toDep())
+//    runtimeOnly(libs.plugins.sqldelight.toDep())
 
     // Room
     implementation(libs.plugins.room.toDep())
-    runtimeOnly(libs.plugins.room.toDep())
+//    runtimeOnly(libs.plugins.room.toDep())
 
     // Ktor
     runtimeOnly(libs.plugins.ktor.toDep())
 
     // Http client generator
     implementation(libs.plugins.ktorfit.toDep())
-    runtimeOnly(libs.plugins.ktorfit.toDep())
+//    runtimeOnly(libs.plugins.ktorfit.toDep())
 
     // GraphQL
     implementation(libs.plugins.apollo3.toDep())
-    runtimeOnly(libs.plugins.apollo3.toDep())
+//    runtimeOnly(libs.plugins.apollo3.toDep())
 }
 
 gradlePlugin {
