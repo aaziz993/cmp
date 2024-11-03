bf2f5ba53d068baeca8fe903e55daf1c4db87461
Aziz <a.atoev93@gmail.com>
2024-11-03 19:52:48 +0500

    Initial Commit

--- cmp-lib/src/commonMain/kotlin/ai/tech/core/misc/type/serializer/DpJsonSerializer.kt
+++ cmp-lib/src/commonMain/kotlin/ai/tech/core/misc/type/serializer/DpJsonSerializer.kt
@@ -1,16 +1,16 @@
 package ai.tech.core.misc.type.serializer
 
+import ai.tech.core.misc.type.serializer.primitive.PrimitiveFloatSerializer
 import ai.tech.core.misc.type.serializer.primitive.PrimitiveStringSerializer
 import androidx.compose.ui.unit.Dp
 import kotlinx.serialization.Serializable
 
 public object DpJsonSerializer :
-    PrimitiveStringSerializer<Dp>(
+    PrimitiveFloatSerializer<Dp>(
         Dp::class,
         Dp::value,
         ::Dp,
     )
-
 
 public typealias DpSerial = @Serializable(with = DpJsonSerializer::class) Dp
 
