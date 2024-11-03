69e66405e4e9143cd52ce2412e91e93fba0d68c0
Aziz <a.atoev93@gmail.com>
2024-11-03 20:24:29 +0500

    Initial Commit

--- cmp-lib/src/commonMain/kotlin/ai/tech/core/misc/type/serializer/TopAppBarColorsJsonSerializer.kt
+++ cmp-lib/src/commonMain/kotlin/ai/tech/core/misc/type/serializer/TopAppBarColorsJsonSerializer.kt
@@ -1,19 +1,19 @@
-package ai.tech.core.misc.type.serializer
-
-import ai.tech.core.misc.type.serializer.primitive.PrimitiveStringSerializer
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TopAppBarColors
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-
-@OptIn(ExperimentalMaterial3Api::class)
-public class TopAppBarColorsJsonSerializer :
-    PrimitiveStringSerializer<TopAppBarColors>(TopAppBarColors::class, { null as TopAppBarColors }, { "" }) {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("TopAppBarDefaults", PrimitiveKind.STRING)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-public typealias TopAppBarColorsJson = @Serializable(with = TopAppBarColorsJsonSerializer::class) TopAppBarColors
+//package ai.tech.core.misc.type.serializer
+//
+//import ai.tech.core.misc.type.serializer.primitive.PrimitiveStringSerializer
+//import androidx.compose.material3.ExperimentalMaterial3Api
+//import androidx.compose.material3.TopAppBarColors
+//import kotlinx.serialization.Serializable
+//import kotlinx.serialization.descriptors.PrimitiveKind
+//import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+//import kotlinx.serialization.descriptors.SerialDescriptor
+//
+//@OptIn(ExperimentalMaterial3Api::class)
+//public class TopAppBarColorsJsonSerializer :
+//    PrimitiveStringSerializer<TopAppBarColors>(TopAppBarColors::class, { null as TopAppBarColors }, { "" }) {
+//    override val descriptor: SerialDescriptor
+//        get() = PrimitiveSerialDescriptor("TopAppBarDefaults", PrimitiveKind.STRING)
+//}
+//
+//@OptIn(ExperimentalMaterial3Api::class)
+//public typealias TopAppBarColorsJson = @Serializable(with = TopAppBarColorsJsonSerializer::class) TopAppBarColors
