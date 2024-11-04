5739badebdd73472e90b1f0bd3125b4d3a4f33f6
Aziz <a.atoev93@gmail.com>
2024-11-05 00:42:40 +0500

    Initial Commit

--- composeApp/src/commonMain/kotlin/ai/tech/di/module/CommonModule.kt
+++ composeApp/src/commonMain/kotlin/ai/tech/di/module/CommonModule.kt
@@ -1,6 +1,13 @@
 package ai.tech.di.module
 
 import ai.tech.core.data.http.client.createHttpClient
+import ai.tech.core.misc.auth.client.ClientAuthProvider
+import ai.tech.core.misc.auth.client.keycloak.KeycloakAuthProvider
+import ai.tech.core.misc.auth.client.keycloak.KeycloakClient
+import ai.tech.core.misc.location.LocalizationProvider
+import ai.tech.core.misc.location.weblate.WeblateClient
+import ai.tech.core.misc.location.weblate.WeblateLocalizationProvider
+import ai.tech.core.misc.model.config.client.ClientConfig
 import ai.tech.core.presentation.event.navigator.DefaultNavigator
 import ai.tech.core.presentation.event.navigator.Navigator
 import ai.tech.navigation.presentation.Destination
@@ -14,14 +21,23 @@ import org.koin.core.annotation.Single
 
 @Module
 public class CommonModule(private val enableNetworkLogs: Boolean) {
+
     @Single
     public fun provideJson(): Json = Json { isLenient = true; ignoreUnknownKeys = true }
 
     @Single
     public fun provideHttpClient(json: Json): HttpClient = createHttpClient {
         install(ContentNegotiation) {
-            json(json)
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                    explicitNulls = false
+                },
+            )
         }
+
         if (enableNetworkLogs) {
             install(Logging) {
                 logger = Logger.DEFAULT
@@ -32,4 +48,28 @@ public class CommonModule(private val enableNetworkLogs: Boolean) {
 
     @Single
     public fun provideNavigator(): Navigator<Destination> = DefaultNavigator(Destination.HomeGraph.Main)
+
+    @Single
+    public fun provideLocalizationProvider(
+        config: ClientConfig,
+        httpClient: HttpClient,
+    ): LocalizationProvider =
+        WeblateLocalizationProvider(
+                WeblateClient(
+                        httpClient,
+                        config.localization.weblate[config.localizationProvider]!!,
+                ),
+                config.project,
+        )
+
+    @Single
+    public fun provideAuthProvider(
+        config: ClientConfig,
+        httpClient: HttpClient,
+        keyValue: SettingsKeyValue
+    ): ClientAuthProvider = KeycloakAuthProvider(
+        KeycloakClient(httpClient, config.auth.keycloak[config.authProvider]!!),
+        keyValue,
+        config.authProvider,
+    )
 }
